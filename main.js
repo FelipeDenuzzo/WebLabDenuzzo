7f54c5a4deb70e333d5e359668aa2e96def81549
FelipeDenuzzo <152036119+FelipeDenuzzo@users.noreply.github.com>
2025-01-26 10:40:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Phaser 3: Jogo de labirinto responsivo com suporte para dispositivos móveis e controles de toque.
+
 class MainScene extends Phaser.Scene {
   constructor() {
     super({ key: 'MainScene' });
@@ -6,6 +8,10 @@ class MainScene extends Phaser.Scene {
   preload() {
     this.load.image('player', 'assets/player.png');
     this.load.image('goal', 'assets/goal.png');
+    this.load.image('buttonUp', 'assets/buttontop.png');
+    this.load.image('buttonDown', 'assets/buttondown.png');
+    this.load.image('buttonLeft', 'assets/buttonleft.png');
+    this.load.image('buttonRight', 'assets/buttonright.png');
   }
 
   create() {
@@ -132,15 +138,15 @@ class MainScene extends Phaser.Scene {
     const speed = 200;
     this.player.setVelocity(0);
 
-    if (this.cursors.left.isDown) {
+    if (this.cursors.left.isDown || buttonStates.left) {
       this.player.setVelocityX(-speed).setRotation(Math.PI);
-    } else if (this.cursors.right.isDown) {
+    } else if (this.cursors.right.isDown || buttonStates.right) {
       this.player.setVelocityX(speed).setRotation(0);
     }
 
-    if (this.cursors.up.isDown) {
+    if (this.cursors.up.isDown || buttonStates.up) {
       this.player.setVelocityY(-speed).setRotation(-Math.PI / 2);
-    } else if (this.cursors.down.isDown) {
+    } else if (this.cursors.down.isDown || buttonStates.down) {
       this.player.setVelocityY(speed).setRotation(Math.PI / 2);
     }
   }
@@ -166,6 +172,62 @@ class MainScene extends Phaser.Scene {
   }
 }
 
+class UIScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'UIScene', active: true });
+  }
+
+  preload() {
+    this.load.image('buttonUp', 'assets/buttontop.png');
+    this.load.image('buttonDown', 'assets/buttondown.png');
+    this.load.image('buttonLeft', 'assets/buttonleft.png');
+    this.load.image('buttonRight', 'assets/buttonright.png');
+  }
+
+  create() {
+    const buttonSize = Math.min(this.scale.width, this.scale.height) / 10;
+    const screenWidth = this.scale.width;
+    const screenHeight = this.scale.height;
+
+    this.controlButtons = {
+      up: this.add
+        .image(screenWidth / 2, screenHeight - buttonSize * 2.5, 'buttonUp')
+        .setInteractive()
+        .setDisplaySize(buttonSize, buttonSize),
+      down: this.add
+        .image(screenWidth / 2, screenHeight - buttonSize / 2, 'buttonDown')
+        .setInteractive()
+        .setDisplaySize(buttonSize, buttonSize),
+      left: this.add
+        .image(screenWidth / 2 - buttonSize, screenHeight - buttonSize * 1.5, 'buttonLeft')
+        .setInteractive()
+        .setDisplaySize(buttonSize, buttonSize),
+      right: this.add
+        .image(screenWidth / 2 + buttonSize, screenHeight - buttonSize * 1.5, 'buttonRight')
+        .setInteractive()
+        .setDisplaySize(buttonSize, buttonSize),
+    };
+
+    Object.values(this.controlButtons).forEach(button => {
+      button.setScrollFactor(0);
+    });
+
+    this.controlButtons.up.on('pointerdown', () => (buttonStates.up = true));
+    this.controlButtons.up.on('pointerup', () => (buttonStates.up = false));
+
+    this.controlButtons.down.on('pointerdown', () => (buttonStates.down = true));
+    this.controlButtons.down.on('pointerup', () => (buttonStates.down = false));
+
+    this.controlButtons.left.on('pointerdown', () => (buttonStates.left = true));
+    this.controlButtons.left.on('pointerup', () => (buttonStates.left = false));
+
+    this.controlButtons.right.on('pointerdown', () => (buttonStates.right = true));
+    this.controlButtons.right.on('pointerup', () => (buttonStates.right = false));
+  }
+}
+
+const buttonStates = { up: false, down: false, left: false, right: false };
+
 const config = {
   type: Phaser.AUTO,
   width: window.innerWidth,
@@ -177,7 +239,7 @@ const config = {
       debug: false,
     },
   },
-  scene: [MainScene],
+  scene: [MainScene, UIScene],
   scale: {
     mode: Phaser.Scale.RESIZE,
     autoCenter: Phaser.Scale.CENTER_BOTH,
